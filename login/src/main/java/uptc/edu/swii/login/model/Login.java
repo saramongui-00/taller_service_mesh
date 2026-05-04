28f05ebe2d91882e8a92c0986d195d6035983bef
saraa <saraalmongui@gmail.com>
2026-05-04 16:31:10 -0500

    servicio login terminado

--- login/src/main/java/uptc/edu/swii/login/model/Login.java
+++ login/src/main/java/uptc/edu/swii/login/model/Login.java
@@ -7,6 +7,10 @@ import jakarta.persistence.*;
 public class Login {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
     @Column(name = "customerid")
     private String customerid;
 
@@ -15,14 +19,33 @@ public class Login {
 
     public Login(){}
 
-    public Login(String customerid, String password) {
+    public Login(Long id, String customerid, String password) {
+        this.id = id;
         this.customerid = customerid;
         this.password = password;
     }
 
-    public String getCustomerid() { return customerid; }
-    public void setCustomerid(String customerid) { this.customerid = customerid; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(String customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
